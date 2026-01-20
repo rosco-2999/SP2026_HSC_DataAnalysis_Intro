8d0b2ef17be063f37efd1380f225565240d7044e
Mallory Milstead <mallorymilstead@gmail.com>
2026-01-20 12:54:07 -0500

    Update group1.py

--- group1.py
+++ group1.py
@@ -1,1 +1,2 @@
 # Create a program that lists all the names of the passengers that survived and another list that shows all the names of the passengers that survived.
+# Create a plot to show the number of females that survived vs the number of males that survived.
